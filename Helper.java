fa3c785098cf3ab512bd909cb69be835df4f0076
surjya078 <106722591+surjya078@users.noreply.github.com>
2024-04-04 21:08:52 +0530

    Add files via upload

---
+++ Helper.java
@@ -0,0 +1,16 @@
+package com.mycompany.ecommerce.helper;
+
+public class Helper {
+	public static String get10Words(String desc) {
+		String[] strs = desc.split(" ");
+		if (strs.length > 10) {
+			String res = "";
+			for (int i = 0; i < 10; i++) {
+				res += strs[i]+" ";
+			}
+			return (res + "....");
+		} else {
+			return (desc + "....");
+		}
+	}
+}
